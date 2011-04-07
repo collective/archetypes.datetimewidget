e7a868b6688c297d51ce2651ad6932af2a8d2549
Vincent Fretin <vincent.fretin@gmail.com>
2011-04-07 19:52:34 +0200

    cleanup tests setup

--- archetypes/datetimewidget/tests/base.py
+++ archetypes/datetimewidget/tests/base.py
@@ -1,7 +1,3 @@
-import unittest
-
-from Testing import ZopeTestCase as ztc
-
 from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase as ptc
@@ -10,22 +6,26 @@ from Products.PloneTestCase.layer import PloneSite
 # setup test content types
 from Products.GenericSetup import EXTENSION, profile_registry
 
+
 profile_registry.registerProfile('DatetimeWidget_examples',
     'DatetimeWidget Example Content Types',
     'Extension profile including Archetypes example content types',
     'profiles/examples',
     'archetypes.datetimewidget',
     EXTENSION)
-ptc.setupPloneSite(extension_profiles=['archetypes.datetimewidget:DatetimeWidget_examples',])
+ptc.setupPloneSite(
+    extension_profiles=['archetypes.datetimewidget:DatetimeWidget_examples',])
+
 
 import archetypes.datetimewidget
+
 
 class TestCase(ptc.PloneTestCase):
     class layer(PloneSite):
         @classmethod
         def setUp(cls):
             fiveconfigure.debug_mode = True
-            ztc.installPackage('archetypes.datetimewidget', quiet=0)
+            ptc.installPackage('archetypes.datetimewidget', quiet=0)
             zcml.load_config('configure.zcml',
                              archetypes.datetimewidget)
             fiveconfigure.debug_mode = False
